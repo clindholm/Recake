a759208438fc7144808931626e8ca1ff32c0591f
Cal <lindholm.calle@gmail.com>
2020-04-28 17:48:56 +0200

    Update release.sh

--- release.sh
+++ release.sh
@@ -5,5 +5,4 @@ NODE_ENV=production npm run deploy --prefix assets
 MIX_ENV=prod mix phx.digest
 MIX_ENV=prod mix release
 cd _build/prod/rel/bygg_app/
-tar czf ./bygg_app.tar.gz .
-mv ./bygg_app.tar.gz ../../../../tmp/bygg_app.tar.gz
+tar czf ../../../../tmp/bygg_app.tar.gz .
