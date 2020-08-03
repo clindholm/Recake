383e27804083c98eb42f11b7b1dc8f027b72b7d0
Cal <github.com@calle.space>
2020-08-03 12:33:10 +0200

    Clean up recruits list

--- release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir -p tmp/
-
-NODE_ENV=production npm run deploy --prefix assets
-MIX_ENV=prod mix phx.digest
-MIX_ENV=prod mix release
-cd _build/prod/rel/recake/
-tar czf ../../../../tmp/recake.tar.gz .
